e1aa7238086cfd9b7ac00a0d9b1efd11ed94cbc0
Moustapha <mouhamous22@gmail.co>
2021-09-15 11:08:55 +0200

    search result get lines & languages

--- pages/search.vue
+++ pages/search.vue
@@ -2,12 +2,24 @@
     <div>
       <div>Results</div>
 
-      <div>
-          <div v-for="item in results" :key="item.id">
-            - <a :href="item.html_url">  {{ item.full_name }}</a> : <span >lines  </span> 
+      <div v-for="item in results" :key="item.id">
+          <div >
+            - <a :href="item.html_url">  {{ item.full_name }}</a> :<span v-if="lines(item.id)">lines {{lines(item.id).lines > 0 ? lines(item.id).lines : 0}} </span> 
+          </div>
+
+          <!-- Languages section -->
+          <div>
+            <p>
+                Languages: <span v-if="languages(item.id)">
+                <em v-for="(value, key) in languages(item.id).languages" :key="key" >{{ key }},</em>
+              </span>
+            </p> 
           </div>
       </div>
+      
     </div>
+
+    
   
 </template>
 
@@ -24,7 +36,7 @@ export default {
 
   computed:{
     ...mapGetters({
-      getItems:'repos/getItems',
+      //getItems:'repos/getItems',
       getLines: 'repos/getLines',
       getLanguage: 'repos/getLanguages'
     }),
@@ -40,6 +52,15 @@ export default {
       let result = items.filter(repos => repos.full_name.includes(word))
       //console.log(result)
       this.results = result
+    },
+
+    lines(item_id){
+      let data = this.getLines.find( ({ id }) => id === item_id )
+      return data
+    },
+    languages(item_id){
+      let data = this.getLanguage.find( ({ id }) => id === item_id )
+      return data
     }
 
   },
